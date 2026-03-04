814cdba209a62e6a9153873958c83b6c02a4317c
Jeff Rynhart <jeff.rynhart@du.edu>
2026-03-04 10:07:19 -0700

    added random channel on page load, added dial click event to separate function

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function getRandomChannel() {
     "channel2.png",
     "channel3.png",
     "channel4.png",
-    // "./img/channel5.png"
   ];
 
   const randomIndex = Math.floor(Math.random() * channels.length);
@@ -12,18 +11,23 @@ function getRandomChannel() {
 }
 
 function updateChannel() {
+  const channelImage = document.getElementById("channel-image");
+
+  // get a different channel each time 
+  let randomChannel, currentChannel;
+  do {
+    randomChannel = getRandomChannel();
+    currentChannel = channelImage.src.substring(channelImage.src.lastIndexOf("/") + 1);
+    
+  } while (randomChannel === currentChannel);
+  
+  // set the screen to the new channel image
+  channelImage.src = "img/" + randomChannel;
+}
+
+function clickDial() {
   const clickAudio = document.getElementById("click-audio");
   clickAudio.play();
 
-  const channelImage = document.getElementById("channel-image");
-
-  // get a different channel each time the button is clicked
-  let rand, currentChannel;
-  do {
-    rand = getRandomChannel();
-    currentChannel = channelImage.src.substring(channelImage.src.lastIndexOf("/") + 1)
-  } while (rand === currentChannel);
-  
-  // set the screen to the new channel image
-  channelImage.src = "img/" + rand;
+  updateChannel();
 }
